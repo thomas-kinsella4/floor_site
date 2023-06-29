2abba72612f9775914bb53194bc151f5c79c8952
Gary Flugel <flugelg@gmail.com>
2023-06-28 20:27:58 -0400

    Cleaned up code and split into components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='bgimg1'>
+            <div className='caption'>
+                <span className='border'>WALLY FLOORS</span>
+            </div>
+        </div>
+    )
+}
+
+export default Home
